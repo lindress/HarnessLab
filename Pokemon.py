fa03ac2215f33576cc18cbf42156b654c29ab249
Anne Wesley <47316312+lindress@users.noreply.github.com>
2024-04-28 14:50:00 -0400

    put pokemon team in logs

--- Pokemon.py
+++ Pokemon.py
@@ -1,5 +1,9 @@
 import time
 import random
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 team = []
 types = ["Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bug", "Ghost", "Steel", "Fire", "Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy", "Flying", "Electric", "Psychic", "Bug", "Rock", "Ice", "Fairy", "Dragon", "Steel", "Ghost"]
@@ -18,12 +22,12 @@ def rpokemon(type):
 
 # due to dual type pokemon, we need to check if the pokemon is already in the list,
 # if it is select another pokemon, else add it to the team and remove the type from the list.
-def check(pokemon, ptype):
+def check(pokemon, ptype, i):
     if pokemon in team:
         rpokemon(ptype)
         check(pokemon)
     else:
-        team.append(pokemon)
+        team.append(f"{i}. {pokemon}")
         types.remove(ptype)
 
 
@@ -32,10 +36,10 @@ def main():
     for i in range(team_size):
         ptype = rtype()
         pokemon = rpokemon(ptype)
-        check(pokemon, ptype)
+        check(pokemon, ptype, i+1)
 
     for mon in team:
-        print(mon)
+        logging.info(mon)
 
     while True:
         time.sleep(3600)  # Sleep for 1 hour
